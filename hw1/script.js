2f80e32539c4604821eadd721c81f0941a9b0959
fr1kols <frkols5@gmail.com>
2024-06-19 16:34:11 +0300

    123
    123

---
+++ hw1/script.js
@@ -0,0 +1,46 @@
+ const age = 16;
+ console.log(age);
+//2
+
+const userName = "Stanislav";
+console.log(userName);
+//3
+
+const isStudent = true;
+console.log(isStudent);
+//4
+
+const isString = "hello";
+console.log(isString);
+//5
+
+let myNumber = 10;
+myNumber += 10;
+console.log(myNumber);
+//6
+
+const myNull = null;
+console.log(myNull);
+//7
+
+// const name = prompt('Введіть ваше і*мя');
+// alert('Вітаємо,' + name + '!');
+//8
+
+// const nameUser = confirm('Ви пітверджуєте дію?');
+// if(nameUser){
+//     alert('Дякую!')
+// }
+// else{
+//     alert('Дію відміненно')
+// }
+//9
+
+alert('Увага дія небезпечна!');
+const dangerous = confirm('Ви пітверджуєте дію?');
+if(dangerous){
+    alert('Дякую за пітвердження!')
+}
+else{
+    alert('Дію відміненно')
+}
---
+++ script.js
@@ -0,0 +1,57 @@
+// "use strict"
+// сутність.length
+
+
+console.log('Stanislav'.length);
+console.log('Stanislav'.toUpperCase());
+
+// var a = 10;
+
+// const b = 20;
+
+// let c = 25;
+
+// c = 30;
+
+// let age = 25;
+// age = 30;
+// console.log(age);
+
+
+// const a = 5;
+// const b = 25.5;
+
+// const c = 'hello';
+
+// const d = true;
+
+// const isModalOpen = false;
+
+// const selectedProduct = null;
+// let userName;
+// console.log(userName);
+
+// console.log(typeof a);
+// console.log(typeof userName);
+
+// const message = 'Hello GoITeens';
+// console.log(message);
+
+// const userName = 'Nick';
+// console.log("Username is", userName)
+
+// alert(message);
+
+// const message = confirm("Чи згодні з умовами?");
+// console.log(message);
+
+// const message = prompt("Вкажіть ваш вік");
+// console.log(message);
+
+// let groupName = '19';
+// console.log("Я навчаюся в GoITeens в групі:", groupName)
+
+let myAge = 15;
+let friendAge =16
+console.log(myAge + friendAge);
+console.log(typeof myAge)
