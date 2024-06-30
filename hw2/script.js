b3c75c7c8812332d36c17fddb209e9a25c241d9d
fr1kols <frkols5@gmail.com>
2024-06-30 17:37:24 +0300

    Домашня робота №2
    Домашня робота №2

---
+++ hw2/script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const c = 20;
+const f = 1.8 * c + 32;
+console.log(f);
+
+// 2
+const days = 30;
+const hours = days * 24;
+const minutes = hours * 60;
+console.log(hours);
+console.log(minutes);
+
+// 3
+let h = 100;
+let e = 100;
+h -= 25;
+e -= 30;
+console.log(h)
+console.log(e)
+
+// 4
+let price = 100;
+let sum  = price - price * 0.1;
+// let sum = (price / 100) * 90;
+console.log(sum);
+
+// 5 
+const n = 3.14;
+console.log(Math.floor(n));
+
+// 6
+const v = '5.741hg11';
+console.log(parseFloat(v));
+
+// 7 
+const x = '3.1524pppq1'
+console.log(parseInt(x));
+
+//8
+
+const a = 16;
+console.log(Math.sqrt(a));
+
+// 9 
+const num = 20;
+const string = '15kj';
+console.log(parseInt(string));
+console.log(num.toString());
--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 // сутність.length
 
 
-console.log('Stanislav'.length);
-console.log('Stanislav'.toUpperCase());
+// console.log('Stanislav'.length);
+// console.log('Stanislav'.toUpperCase());
 
-// var a = 10;
+// // var a = 10;
 
-// const b = 20;
+// // const b = 20;
 
 // let c = 25;
 
@@ -51,7 +51,51 @@ console.log('Stanislav'.toUpperCase());
 // let groupName = '19';
 // console.log("Я навчаюся в GoITeens в групі:", groupName)
 
-let myAge = 15;
-let friendAge =16
-console.log(myAge + friendAge);
-console.log(typeof myAge)
+// let myAge = 15;
+// let friendAge =16
+// console.log(myAge + friendAge);
+// console.log(typeof myAge)
+
+
+const c = '20';
+console.log(Number(c));
+
+const d = 'qwe';
+console.log(Number(d));
+console.log(typeof Number(d));
+
+console.log(Number.parseInt('10px'));
+
+console.log(Number.parseInt('10px'));
+console.log(Number.parseInt('15qwe211'));
+console.log(Number.parseInt('14.45qwe'));
+console.log(Number.parseInt('qwe'));
+
+console.log(Number.parseFloat('10px'));
+console.log(Number.parseFloat('12qwe74'));
+console.log(Number.parseFloat('12.24qwe'));
+
+const x = Number('25');
+console.log(Number.isNaN(x));
+
+const y = Number('qwqwq');
+console.log(Number.isNaN(y));
+
+0.3 + 0.5 
+console.log(0.1 + 0.3 === 0.4); // 0.4000000001 === 0.4
+console.log(0.1 + 0.3 );
+
+(0.1*10 + 0.3*10) / 10
+
+console.log(Math.floor(2.8));
+console.log(Math.ceil(3.4));
+console.log(Math.round(1.2));
+console.log(Math.round(1.5));
+
+console.log(Math.max(20, 100, 47, 124, 99));
+
+console.log(Math.min(85, 24, 78, 12));
+
+console.log(Math.pow(2, 3));
+
+console.log(Math.random() * (10 - 1)) + 1;
