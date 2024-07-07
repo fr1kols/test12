c12182a1b82f77eff5b12a29ccfdbe0aa87fcd0f
fr1kols <frkols5@gmail.com>
2024-07-07 15:34:22 +0300

    hw-3
    hw-3

---
+++ hw3/script.js
@@ -0,0 +1,24 @@
+const result = 5 + 5 + '5';
+console.log(result);
+
+// 2
+const email = "alibaba@gmail.com";
+console.log(email.includes('@'));
+console.log(email.length);
+
+
+// 3
+const name = 'Stanislav';
+const fullName = 'My, name, is ' + name;
+console.log(fullName);
+
+const fulName = `My, name, is ${name}`;
+console.log(fulName);
+
+// 4
+
+const UserName = 'Олександро';
+const payment = '300';
+
+alert(`Дякуємо, ${UserName}! До сплати ${payment} гривень.`);
+
